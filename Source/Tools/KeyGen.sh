96e7c2e2b5a75d60ca75ef2dbae7913b17e9bc44
Julian Löhr <julian.loehr@outlook.com>
2016-11-28 14:24:35 +0100

    Change back to previous directory

--- Source/Tools/KeyGen.sh
+++ Source/Tools/KeyGen.sh
@@ -9,9 +9,12 @@ else
 	node_data_dir=$npm_package_config_devel_datadir
 fi
 
+workdir=$( pwd )
 mkdir -p $node_data_dir
 cd $node_data_dir
 
 if [ ! -f $npm_package_config_ssl_key_name -o ! -f $npm_package_config_ssl_cert_name ] ; then
 	openssl req -x509 -nodes -newkey rsa:2048 -keyout $npm_package_config_ssl_key_name -out $npm_package_config_ssl_cert_name -subj "/CN=*"
 fi
+
+cd $workdir
