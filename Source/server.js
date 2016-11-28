e7c18cc08b6622700e9c6900638a0ea2e378f35c
Julian Löhr <julian.loehr@outlook.com>
2016-11-28 13:48:38 +0100

    Add persistent node data folder on host system

--- Source/server.js
+++ Source/server.js
@@ -1,5 +1,10 @@
 var express = require('express')
 var app = express()
+const production = (process.env.NODE_ENV == 'production');
+console.log('Node is running in ' + (production ? 'production' : 'development') + ' mode!');
+
+const datadir = production ? process.env.npm_package_config_production_datadir : process.env.npm_package_config_devel_datadir
+console.log('Node data directory: ' + datadir);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
