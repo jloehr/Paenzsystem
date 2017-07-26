43b6f4584389ecdb82ed9e3fd034e5064a520b2d
Julian Löhr <julian.loehr@outlook.com>
2017-07-26 02:05:57 +0200

    Make script name all lower case

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+#
+# Deployment script for Pänzsystem.
+# Downloads start, stop and upgrade scripts. 
+# Then runs Upgrade to pull and setup docker images.
+# 
+# Usage:
+#  'curl -sSL https://raw.githubusercontent.com/jloehr/Paenzsystem/master/deploy.sh | sh'
+# or:
+#  'wget -qO- https://raw.githubusercontent.com/jloehr/Paenzsystem/master/deploy.sh | sh'
+#
+
+scripts="paenzsystem"
+url='https://raw.githubusercontent.com/jloehr/Paenzsystem/master/Docker/'
+
+echo 'Checking dependencies...'
+
+if ! command -v docker >/dev/null 2>&1 ; then
+	echo >&2 "Docker not found! Please install Docker (eg. 'curl -sSL https://get.docker.com/ | sh')."
+	exit 1
+fi
+
+if ! command -v wget >/dev/null 2>&1 ; then
+	echo >&2 "Missing Wget!"
+	exit 1
+fi
+
+echo 'Downloading scripts...'
+
+for script in $scripts ; do
+	wget  $url$script
+	chmod u+x $script
+done
+
+echo 'Run upgrade to pull and create containers...'
+
+. ./paenzsystem upgrade
