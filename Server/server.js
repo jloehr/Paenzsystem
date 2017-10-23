290ec8723d9c2f4747aecfb3d077d00ea28005ab
Julian Löhr <julian.loehr@outlook.com>
2017-10-24 01:12:52 +0200

    Switch to mongoose

--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,25 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+var app = express();
 var https = require('./services/https.js');
 var http = require('./services/http.js');
-var bodyParser = require('body-parser');
-var db = require('./services/mongodb.js');
 var config = require('./config/config.js');
+
+var citizens = require('./routes/citizens.js');
 
 console.log('Node is running in ' + (config.production ? 'production' : 'development') + ' mode!');
 console.log('Node data directory: ' + config.datadir);
 
+const url = `mongodb://${config.db_host}:27017/paenzsystem`;
+mongoose.connect(url, { useMongoClient: true });
+
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function startServer(db)
-{
-	http.start();
-	https.start(app, config);
+app.use(express.static('public'));	
+app.use(citizens.path, citizens.router);
 
-	app.use(express.static('public'));
-}
-
-db.connect(startServer, config);
+http.start();
+https.start(app, config);
