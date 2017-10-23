ba9149433434fcabc1c61b5f6b4bf1a5c9fb9759
Julian Löhr <julian.loehr@outlook.com>
2017-10-23 23:38:43 +0200

    Add body-parser for parsing json data

--- Server/server.js
+++ Server/server.js
@@ -3,11 +3,15 @@ var app = express();
 
 var https = require('./services/https.js');
 var http = require('./services/http.js');
+var bodyParser = require('body-parser');
 var db = require('./services/mongodb.js');
 var config = require('./config/config.js');
 
 console.log('Node is running in ' + (config.production ? 'production' : 'development') + ' mode!');
 console.log('Node data directory: ' + config.datadir);
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 function startServer(db)
 {
