f5802e09b71b93df8778de7813931caa81590bc8
Julian Löhr <julian.loehr@outlook.com>
2017-10-24 02:13:14 +0200

    Add "/api/" prefix

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var app = express();
+var api = express.Router();
 var https = require('./services/https.js');
 var http = require('./services/http.js');
 var config = require('./config/config.js');
@@ -12,14 +13,21 @@ var citizens = require('./routes/citizens.js');
 console.log('Node is running in ' + (config.production ? 'production' : 'development') + ' mode!');
 console.log('Node data directory: ' + config.datadir);
 
+// Create Database Connection
 const url = `mongodb://${config.db_host}:27017/paenzsystem`;
 mongoose.connect(url, { useMongoClient: true });
 
+// Enable Middleware for JSON parsing
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set up Endpoints
 app.use(express.static('public'));	
-app.use(citizens.path, citizens.router);
+app.use('/api', api);
 
+// Set up API Endpoints
+api.use(citizens.path, citizens.router);
+
+// Start listening
 http.start();
 https.start(app, config);
