654dc2e60ad83838dfe9686a9f1ea35a2386ab22
Julian Löhr <julian.loehr@outlook.com>
2017-10-25 02:16:56 +0200

    Change starting id

--- Server/models/counter.js
+++ Server/models/counter.js
@@ -3,7 +3,7 @@ var Schema  = mongoose.Schema;
 
 var CounterSchema = new Schema({
       _id: { type: String, required: true },
-      Counter: { type: Number, default: 0 }
+      Counter: { type: Number, default: 170000 }
 }); 
 
 module.exports = mongoose.model('Counter', CounterSchema);
